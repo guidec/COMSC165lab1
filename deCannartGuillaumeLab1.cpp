83405898b6d824e606cbd08bde948dc4ad6139b6
Guillaume de Cannart <guillaumedecannart@gmail.com>
2019-03-06 22:52:58 -0800

    added functions for menu, patient addition, removal and selection

--- deCannartGuillaumeLab1.cpp
+++ deCannartGuillaumeLab1.cpp
@@ -21,24 +21,29 @@ Course: COMSC-165-2405
 #include<string>
 #include<sstream>
 #include<vector>
+#include<iomanip>
 
 using namespace std;
 
-const int DAILY_RATE = 95.99;
+const double DAILY_RATE = 95.99;
+const int ADMIN_CODE = 1111;
 
 class PatientAccount {
 
-public:
+private:
 	
 	string name;
 	int age;
 	string address;
 	int ssn;
-	int days, roomCost;
-	
+	int days;
+	double roomCost;
+
+public:
+
 	void setInfo() {
 
-		cout << "New patient account. Enter the following information: "
+		cout << endl << "New patient account. Enter the following information: "
 			<< endl;
 
 		string input;
@@ -64,8 +69,10 @@ public:
 		roomCost = days * DAILY_RATE;
 	}
 
+	string getName() {
+		return name;
+	}
 	//function to print out all content 
-
 
 	//vector to store surgeries.
 
@@ -76,16 +83,59 @@ public:
 
 };
 
+//patient selection display
+int selectPatient(vector<PatientAccount> patientDB) {
+	int patientIndex;
+	string input;
 
+	cout << "DVC Hospital Patient List & ID Number" << endl;
+	cout << "_____________________________________" << endl;
+
+	for (int i = 1; i <= patientDB.size(); i++) {
+		patientDB.at(i).getName();
+		cout << i << ": " << setw(4) << patientDB.at(i).getName() << endl;
+	}
+
+	cout << "Please select your ID Number: ";
+	getline(cin, input);
+	stringstream(input) >> patientIndex;
+
+	return patientIndex;
+}
+
+
+//create a new patient
 void newPatient(vector<PatientAccount> &patientDB) {
 	PatientAccount newPatient;
 	newPatient.setInfo();
 	patientDB.push_back(newPatient);
-
 }
 
+
+//remove an element from the patient vector
+void removePatient(vector<PatientAccount> &patientDB, int patientIndex) {
+	//patientDB.erase(patientIndex);
+}
+
+
+//useless check to make sure users dont delete data
+void adminAccess() {
+
+	string input;
+	int userCode;
+	cout << "Patient removal. Please enter admin code: ";
+	getline(cin, input);
+	stringstream(input) >> userCode;
+
+	while (userCode != ADMIN_CODE) {
+		cout << "Invalid Admin code entered. Try again: ";
+	}
+}
+
+//Starts teh menu selection
 void startMenu(vector<PatientAccount> &patientDB) {
 
+	int patientIndex;
 	string input;
 	int selection;
 
@@ -95,7 +145,7 @@ void startMenu(vector<PatientAccount> &patientDB) {
 	cout << "3. Delete Patient Record." << endl;
 	cout << "4. Add surgery billables to patient record." << endl;
 	cout << "5. Add pharmacy billables to patient record." << endl;
-	cout << endl << "Awaiting input: ";
+	cout << "...Awaiting input: ";
 
 	getline(cin, input);
 	stringstream(input) >> selection;
@@ -105,33 +155,29 @@ void startMenu(vector<PatientAccount> &patientDB) {
 		newPatient(patientDB);
 		break;
 	case 2:
-		//printRecord();
+		patientIndex = selectPatient(patientDB);
+		// print out the patient bill 
+		break;
 	case 3:
-		//deleteRecord();
-		cout << "hi";
+		//remove patient record 
+		adminAccess();
+		patientIndex = selectPatient(patientDB);
+		removePatient(patientDB, patientIndex);
+
 	}
 }
 
 int main()
 {
-	string name;
+	string name, input;
 	// create a new patient
 	vector<PatientAccount> patientDB;
 
+	while (1) {
 
-
-	//start user menu upon program startup
-	/*while (1){
 		startMenu(patientDB);
+
 	}
-	*/
-
-	startMenu(patientDB);
-
-	//example retrieve from hostpital database
-
-	//name = patientDB.at(0).name;
-	//cout << name;
 
 	return 0;
 }
